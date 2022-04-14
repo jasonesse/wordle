9f6cc643c119478687effac4fb684c3b4e2c3053
jasonessecodes <jasonessecodes@gmail.com>
2022-04-14 12:03:42 -0400

    added words, definitions and scrabble score

--- getlist.py
+++ getlist.py
@@ -1,6 +1,9 @@
 import requests as r
 import pandas as pd
 import bs4 as bs
+import numpy as np
+import threading
+import logging
 
 def get():
 
@@ -15,31 +18,42 @@ def analyse():
     y = d[(d['Origin'] == 'Middle English') | ( d['Origin'] == 'Old English')].Word
     y.str.upper().to_csv('English-7.csv', header=False, index=False)
 
-def get_definition():
-    with open('English-5.csv', encoding='UTF-8') as f:
-        wrds = f.readlines()
-    definitions = {}
 
-    cnt = 1
-    for wrd in wrds:
+def get_definition(wrds):
+    definitions = {}
+    for cnt, wrd in enumerate(wrds, start=2):
         resp = r.get(f"https://www.onelook.com/?w={wrd}")
         c = resp.content
         soup = bs.BeautifulSoup(c,'lxml')
-        
+
         try:
             p = soup.find('span',{'id':'easel_def__0'}).text
         except:
             p = '[No defintion found.]'
 
-        definitions.update({wrd.replace('\n',''):p})
-        cnt = cnt + 1
+        definitions[wrd.replace('\n','')] = p
         print(cnt)
-    
+
     defs = pd.DataFrame.from_dict(definitions, orient='index')
-    defs.to_csv('English-5-defs.csv', header=False, index=False)
+    defs.to_csv('English-5-defs.csv', mode='a',  header=False, index=True)
         
 
 
 
+if __name__ == '__main__':
+    with open('English-5.csv', encoding='UTF-8') as f:
+        wrds = f.readlines()
 
-get_definition()
+    list_of_words = np.array_split(wrds, 10)
+
+    threads = list()
+    for index in list_of_words:
+        x = threading.Thread(target=get_definition, args=(index,))
+        threads.append(x)
+        x.start()
+
+    for index, thread in enumerate(threads):
+        logging.info("Main    : before joining thread %d.", index)
+        thread.join()
+        logging.info("Main    : thread %d done", index)
+
--- play.py
+++ play.py
@@ -3,6 +3,7 @@ from colorama import Fore
 from colorama import Style
 import sys
 import random
+import os
 
 @dataclass
 class Result():
@@ -10,10 +11,12 @@ class Result():
     gray_count: int=0
     green_count: int=0
     res: list = field(default_factory=list)
+    def save(self):
+        return (' '.join(f'{r.color}{r.letter}' for r in self.res) + f'{Style.RESET_ALL}') 
     def show(self):
-        print(''.join(f'{r.color}{r.letter}' for r in self.res) + f'{Style.RESET_ALL}')
+        print(self.save())
     def show_stat(self):
-        print(f'{Fore.GREEN}Green:{self.green_count}\n{Fore.YELLOW}Yellow:{self.yellow_count}\n{Fore.LIGHTRED_EX}Gray:{self.gray_count}\n{Style.RESET_ALL}')
+        print(f'{Fore.GREEN}Correct:{self.green_count}\n{Fore.YELLOW}Wrong place:{self.yellow_count}\n{Fore.LIGHTRED_EX}No match:{self.gray_count}{Style.RESET_ALL}')
 
 class Letter:
     def __init__ (self,letter, color=Fore.YELLOW):
@@ -30,32 +33,95 @@ class Letter:
         return f'{self.color}{self.letter}{Style.RESET_ALL}'
 
 
+def get_scrabble_score(w: str):
+    score = 0
+    if w.upper() in {'A', 'E', 'I', 'O', 'U', 'L', 'N', 'S', 'T', 'R'}:
+        score += 1
+    if w.upper() in {'D', 'G'}:
+        score += 2
+    if w.upper() in {'B', 'C', 'M', 'P'}:
+        score =+ 3
+    if w.upper() in {'F', 'H', 'V', 'W', 'Y'}:
+        score =+ 4
+    if w.upper() in ('K'):
+        score =+ 5
+    if w.upper() in {'J', 'X'}:
+        score =+ 8
+    if w.upper() in {'Q', 'Z'}:
+        score =+ 10
+
+    return score
+  
+
+def get_words_difficulty_level(scrabble_threshold, words, level):
+    _level_words = []
+        #calculate the scrabble score and return only hard words.
+    for word_def in words:
+        word = word_def.split(',')[0].replace('\n','')
+        scrabble_score = 0
+        for w in word:
+            scrabble_score = scrabble_score + get_scrabble_score(w)
+        if level == 'Hard' and scrabble_score > scrabble_threshold:
+            _level_words.append(f'{word},' + word_def.split(',')[1].replace('\n',''))
+        if level == 'Easy' and scrabble_score <= scrabble_threshold:
+            _level_words.append(f'{word},' + word_def.split(',')[1].replace('\n',''))
+    return _level_words
 
 def play():
-
-    level = input('Level (easy/hard):')
+    level_chosen = False
+    while not level_chosen:
+        hard_mode = input('Do you want to play on Hard mode? (Y/N):')
+        if hard_mode.upper() in ['Y', 'N']:
+            level_chosen = True
 
     with open('English-5.csv', encoding='UTF-8') as wrds:
         words = wrds.readlines()
+    
+    if(hard_mode.upper()=='Y'):
+        words = get_words_difficulty_level(10,words,'Hard')
+    else:
+        words = get_words_difficulty_level(10,words,'Easy')
 
-    answer = words[random.randrange(1,len(words))].replace('\n','')
+
+
+
+    #parse out words and definitions
+    #wordlist = [w.split(',')[0] for w in words]
+
+    idx = random.randrange(1,len(words))
+    answer = words[idx].split(',')[0].replace('\n','')
+    definition = words[idx].split(',')[1].replace('\n','')
+
+    hint_chosen = False
+    hint = 'N'
+    while not hint_chosen and hard_mode.upper() == 'N':
+        hint = input('Do you want the definition? (Y/N):')
+        if hint.upper() in ['Y', 'N']:
+            hint_chosen = True
+
+    if hint.upper() == 'Y':
+        print(definition)
+
+
+    #print(answer)
 
     invalid_letters =[]
-
-    for _ in range(5):
+    all_guesses = []
+    num_turns = 5
+    for turn in range(num_turns):
         valid_input = False
         while(not valid_input):
-            guess = input('Guess:')
+            guess = input(f'Guess ({turn+1}/{num_turns}):')
             guess = guess.upper()
-            if len(guess) == 5:
-                if (f'{guess}\n' not in words):
-                    print('That word isn''t known to me. Try again')
-                else:
-                    valid_input = True                
+            if len(guess) == len(answer):
+                #if (f'{guess}' not in wordlist) and level == 'hard':
+                #    print('That word isn''t known to me. Try again')
+                #TODO check against letters that were missed? 
+                #else:
+                valid_input = True                
             else:
-                print('Must be 5 letters.')
-            
-
+                print(f'Must be {len(answer)} letters.')
+        os.system('cls' if os.name == 'nt' else 'clear')
 
         result = Result()
         for i in range(5):
@@ -75,22 +141,46 @@ def play():
                 result.gray_count +=1
             result.yellow_count = 5 - (result.green_count + result.gray_count)
             result.res.append(guess_letter)
-            if result.green_count == 5:
-                print('You win!')
-                result.show()
-                sys.exit()
-    
-        if level == 'hard':
+
+        all_guesses.append(result.save())
+        for a in all_guesses:
+            print(a)
+
+        if hint.upper() == 'Y':
             result.show_stat()
-        
-        if level == 'easy':
-            result.show()
+
+        if hard_mode.upper() == 'N':
             result.show_stat()
             print('Guessed letters:', ', '.join(set(invalid_letters)))
-    
+
+        if result.green_count == len(answer):
+            print(f'You win! {Fore.GREEN}{answer.upper()}{Style.RESET_ALL} : {definition}')
+            another_turn_chosen = False
+            while not another_turn_chosen:
+                play_again = input('Do you want to play on again? (Y/N):')
+                if play_again.upper() in ['Y', 'N']:
+                    another_turn_chosen = True
+                    return play_again.upper() == 'Y'
+
+        if hint.upper() == 'Y':
+            print(f'Definition: {definition}')
+
     print(f'You lost. The word was {Fore.GREEN}{answer.upper()}{Style.RESET_ALL}')
+    another_turn_chosen = False
+    while not another_turn_chosen:
+        play_again = input('Do you want to play on again? (Y/N):')
+        if play_again.upper() in ['Y', 'N']:
+            another_turn_chosen = True
+            return play_again.upper() == 'Y'
+
+
            
 
 
 if __name__ == '__main__':
-    play()
+    
+    end_game = True
+    while end_game:
+        end_game = play()
+    print('Thanks for playing wordle!')
+    sys.exit()
